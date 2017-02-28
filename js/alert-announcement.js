5fe5dc97a0c6a3a3c013f833741b1cb183863f35
Brian Bailey <bbailey4@iit.edu>
2017-02-28 14:32:49 -0600

    added JS setting for jsonPath to block render array; updated alert-announcement.js to use drupal behavior

--- js/alert-announcement.js
+++ js/alert-announcement.js
@@ -1,3 +1,14 @@
-(function($){
-	console.log('alert-announcement.js loaded');
+/**
+ * @file
+ */
+(function ($) {
+
+  "use strict";
+
+  Drupal.behaviors.alertAnnouncement = {
+    attach: function (context, settings) {
+      console.log('alert-announcement.js loaded with behavior: ' + settings.alertAnnouncement.jsonPath);
+    }
+  };
+
 })(jQuery);
